4db277ea3ec9198a62879f95b6f289ac1e6fcf53
saichaitanya518 <saichaitanya518@gmail.com>
2025-01-14 23:03:23 +0530

    first commit

---
+++ src/main/java/com/example/bookservicediscovery/BookServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookservicediscovery/BookServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
