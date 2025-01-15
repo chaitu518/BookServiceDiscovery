b55ff5defab6bbe3866c760654087414f199a06f
saichaitanya518 <saichaitanya518@gmail.com>
2025-01-15 12:23:54 +0530

    completely service Discovery is implemented

--- src/main/java/com/example/bookservicediscovery/BookServiceDiscoveryApplication.java
+++ src/main/java/com/example/bookservicediscovery/BookServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.example.bookservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BookServiceDiscoveryApplication {
 
     public static void main(String[] args) {
